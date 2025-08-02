b83f0336c09c3e25a51f30a50f2430a38e3f32fa
V-HimaVardhan866 <himavardhanvugranam@gmail.com>
2025-08-02 12:41:39 +0530

    Time: 7 ms (80.88%), Space: 18.1 MB (68.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        res=""
+        for char in s:
+            if char.isalpha() or char.isdigit():
+                res=res+char      
+        print(res)
+        return res==res[::-1]
+        
